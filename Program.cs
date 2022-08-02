be4bcab649e15bb3800c781f6286d38f1c4e1886
enot demiurge <enotstone@ya.ru>
2022-08-02 07:51:50 +0300

    15v2

--- Program.cs
+++ Program.cs
@@ -6,63 +6,59 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp15
 {
-    class Program /* хрень полная, надо сделать базовый класс прогрессия, в нем указать методы
-    {                void SetStart(int x); //устанавливает начальное значение
-                     int GetNext(); // возвращает следующее число ряда
-                     void Reset()// выполняет сброс к начальному значению 
-                     и метод типа прогрессии и все это будет в 4 раза короче 
-                     но мне лень   */
+    class Program /* совершенно безтолоковое решение, нет понимания способов присваивания, обращения, извлечения и использования семантики */ 
+                  
     { 
         static void Main()
         {
             ArithProgression ARProg = new ArithProgression();
             GeomProgression GProg = new GeomProgression();
-            Console.WriteLine("Enter the initial term of the progression, step and number of steps");
-            int it = Convert.ToInt32(Console.ReadLine());
-            ARProg.SetStart(it);
-            GProg.SetStart(it);
+            Console.WriteLine("Enter the initial value of the progression, step and number of steps");
+            int iv = Convert.ToInt32(Console.ReadLine()); //? как грамотно передать тоже значение нескольким методам
+            ARProg.SetStart(iv);
+            GProg.SetStart(iv);
             
-            GProg.Step = ARProg.Step = Convert.ToInt32(Console.ReadLine());
+            //GProg.Step = ARProg.Step = Convert.ToInt32(Console.ReadLine());
+            int step = Convert.ToInt32(Console.ReadLine());
             int NStep = Convert.ToInt32(Console.ReadLine());
             int n = 1;
             do
             {
-                Console.WriteLine("Step {0,4:# ###}: Arithmetic progression: " +
-                    "{1,4:# ###} Geometric progression: " +
-                    "{2,7:# ### ###}", n, ARProg.AP, GProg.GP);
+                Console.WriteLine("Step {0,3:###}: Arithmetic progression: " +
+                    "{1,9:# ### ###} |   Geometric progression: " +
+                    "{2,13:# ### ### ###}", n, ARProg.AP, GProg.GP);
 
-                ARProg.GetNext();
-                GProg.GetNext();
+                ARProg.GetNext(step);
+                GProg.GetNext(step);
                 n++;
             } while (n < NStep);
             Console.ReadKey();
         }
     }
 
-    interface ISeries
+    interface /*interface - предписывает порядок содержания*/ISeries
     {
         void SetStart(int x); //устанавливает начальное значение
-        int GetNext(); // возвращает следующее число ряда
+        int GetNext(int step); // возвращает следующее число ряда
+        //int GetNext();
         void Reset(); // выполняет сброс к начальному значению
     }
     class ArithProgression : ISeries
     {
         public int AP { get; set; }
         int StX;
-        public int Step;
+        //public int Step;
         public void SetStart(int x)
         {
             AP = StX = x;
         }
-        public int GetNext()
+        public int GetNext(int step)
         {
-            //Console.WriteLine("ArithProgression : {0}", AP);
-            return AP += Step;
-            
+            return AP += step;
         }
         public void Reset()
         {
-            AP = StX;
+            AP = StX; //? как грамотно обратиться к начальному значению другого метода того же класса (другого класса)
         }
     }
 
@@ -70,14 +66,14 @@ namespace ConsoleApp15
     {
     public int GP { get; set; }
         int StX;
-        public int Step;
+        //public int Step;
         public void SetStart(int x)
         {
             GP = StX = x;
         }
-        public int GetNext()
+        public int GetNext(int step)
         {
-            return GP *= Step;
+            return GP *= step;
         }
         public void Reset()
         {
@@ -85,3 +81,87 @@ namespace ConsoleApp15
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*                 можно сделать базовый класс прогрессия, в нем указать методы
+                   void SetStart(int x); //устанавливает начальное значение
+                   int GetNext(); // возвращает следующее число ряда
+                   void Reset()// выполняет сброс к начальному значению 
+                   и метод типа прогрессии, все это будет в 4 раза короче 
+                   не успеваю разбираться 
+
+                    interface <имя> предписывает порядок содержания
+                    разобрать использование this, обращение между методами
+
+                   void метод возвращает значение
+                   int метод для возврата, должен использовать return 
+*/
